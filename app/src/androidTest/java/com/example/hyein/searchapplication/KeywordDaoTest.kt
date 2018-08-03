beb37084a728debce41b55d6ac0bf48af42d006c
hyeinqq <hyein@birdview.kr>
2018-08-03 18:32:56 +0900

    dao test를 위해 의존성을 추가하다

--- app/src/androidTest/java/com/example/hyein/searchapplication/KeywordDaoTest.kt
+++ app/src/androidTest/java/com/example/hyein/searchapplication/KeywordDaoTest.kt
@@ -1,22 +1,33 @@
 package com.example.hyein.searchapplication
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.text.method.MetaKeyKeyListener
 import com.example.hyein.searchapplication.database.KeywordDao
 import com.example.hyein.searchapplication.database.SearchDatabase
 import com.example.hyein.searchapplication.model.Keyword
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 class KeywordDaoTest{
 
+    inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
+
     lateinit var database: SearchDatabase
     lateinit var keywordDao: KeywordDao
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
 
     @Before
     fun createDb(){
@@ -33,22 +44,38 @@ class KeywordDaoTest{
 
     @Test
     fun addKeyword(){
-//        val mockObserver = mock(Observer::class.java)
-//        keywordDao.getKeywordStrings().observeForever(mockObserver)
+        val keyword = Keyword("testCase")
+        keywordDao.insert(keyword)
+
+        val keywords =  keywordDao.getAll()
+        assertEquals(keywords.size, 1)
+    }
+
+    @Test
+    fun addDuplicatedKeyword(){
+        val keyword = Keyword("testCase")
+        keywordDao.insert(keyword)
+        keywordDao.insert(keyword)
+
+        val keyword2 = Keyword("testCase")
+        keywordDao.insert(keyword2)
+
+        val keywords =  keywordDao.getAll()
+
+        assertEquals(keywords.size, 1)
+    }
+
+
+    @Test
+    fun getKeywordStrings(){
+        val mockObserver = mock<Observer<List<String>>>()
+        keywordDao.getKeywordStrings().observeForever(mockObserver)
+
+        val keywordStrings = ArrayList(listOf("testCase"))
 
         val keyword = Keyword("testCase")
         keywordDao.insert(keyword)
 
-//        val keywords = keywordDao.getKeywordStrings()
-
-        assertEquals(keyword.name,"testCase")
-//        Assert.assertEquals(keywords.value?.isNotEmpty() , true)
-    }
-
-    @Test
-    fun getKeyword(){
-//        val keywords =  keywordDao.getKeywordStrings()
-
-//        Assert.assertEquals(keywords.value?.size , 4 )
+        Mockito.verify(mockObserver).onChanged(keywordStrings)
     }
 }
